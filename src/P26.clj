1de6dd75d0fccafd53824053fd545a79cb04df27
Joe Littlejohn <joelittlejohn@gmail.com>
2012-02-13 00:27:56 +0000

    P26 (comments & examples)

--- src/P26.clj
+++ src/P26.clj
@@ -1,10 +1,15 @@
-(defn combinations [n s]
+(defn combinations [k s]
   (cond
-    (> n (count s)) nil
-    (= n (count s)) [s]
-    (= 1 n) (map vector s)
+    (> k (count s)) nil        ;not enough items in the sequence to form a valid combination
+    (= k (count s)) [s]        ;only one combination available: all items 
+    (= 1 k) (map vector s)     ;every item (on its own) is a valid combination
     :else (reduce concat (map-indexed 
-            (fn [i x] (map #(cons x %) (combinations (dec n) (drop (inc i) s)))) 
+            (fn [i x] (map #(cons x %) (combinations (dec k) (drop (inc i) s)))) 
             s))))
 
+
 (combinations 3 ['a 'b 'c 'd 'f])
+(count (combinations 3 ['a 'b 'c 'd 'f]))
+
+(combinations 3 ['a 'b 'c 'd 'f 'g 'h 'i 'j 'k 'l 'm 'n 'o 'p 'q 'r 's 't 'u 'v 'w 'x 'y 'z])
+(count (combinations 3 ['a 'b 'c 'd 'f 'g 'h 'i 'j 'k 'l 'm 'n 'o 'p 'q 'r 's 't 'u 'v 'w 'x 'y 'z]))
